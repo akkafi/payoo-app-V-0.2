d53411dc63aab212eade062cc3d0f0919b1050ce
akkafi <abdullacst11@gmail.com>
2026-02-20 17:47:34 +0600

    login page functional

--- script/login.js
+++ script/login.js
@@ -1,1 +1,23 @@
-console.log('login fun')
+// console.log('login fun')
+document.getElementById('login-btn')
+.addEventListener('click' , function(){
+    // console.log('login button clicked');
+    //step 1: get the mobile number input
+    const numberInput = document.getElementById('input-number');
+    const contactNumber = numberInput.value;
+    console.log(contactNumber);
+    //step 2: get the pin input
+    const inputPin = document.getElementById('input-pin');
+    const pin = inputPin.value;
+    console.log(pin);
+    //step 3: match pin & mobile number
+    if(contactNumber == '01234567890' && pin == '1234'){
+
+        //step 3-1: true:::>> alert> homepage
+        alert('login Successful')
+    }else{
+        //step 3-2: false:::>> alert> return
+        alert('login Failed');
+        return;
+    }
+})
